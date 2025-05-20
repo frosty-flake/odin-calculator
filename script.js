942c5b8066b71beddea0894f87c274e004ce62c8
Victor <johnjohnjohnnyjoe@gmail.com>
2025-05-20 20:29:49 +0800

    Create function to parse expression
    Created operator function to parse the input and call the correct
    arithmetic function. The function also catches "division by zero".

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ const subtract = function(a, b) {
 
 const multiply = function(a, b) {
     return a * b;
-}
+};
 
 const divide = function(a, b) {
     return Math.round((a / b) * 1000) / 1000;
-}
+};
+
+const operate = function(num1, operator, num2) {
+    if (operator === "/" && num2 === 0) return "You can't divide by zero!";
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+};
+
