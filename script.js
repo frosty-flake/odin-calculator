ed8b77ea15f8046cd963b85f04efcb67864b6bfc
Victor <johnjohnjohnnyjoe@gmail.com>
2025-05-21 01:56:59 +0800

    Add functionality to calculator
    Added functionality so that pressing buttons populates the display and
    calculates the expression.
    
    Right now, division by zero is correctly caught, but not dealt with
    after being caught. The catch displays "NO!" on the calculator. Need to
    change it so that pressing other buttons clears the text first.
    
    Pressing equal while the expression is not whole is supposed to do
    nothing, but right now it returns "NaN". To fix.

--- script.js
+++ script.js
@@ -1,30 +1,117 @@
+const listOfOperators = ["+", "−", "×", "÷"];
+
 const add = function(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 };
 
 const subtract = function(a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 };
 
 const multiply = function(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 };
 
 const divide = function(a, b) {
-    return Math.round((a / b) * 1000) / 1000;
+    return Number(a) / Number(b);
 };
 
 const operate = function(num1, operator, num2) {
-    if (operator === "/" && num2 === 0) return "You can't divide by zero!";
+    if (operator === "÷" && Number(num2) === 0) return "NO!";
+    let res;
     switch (operator) {
         case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
+            res = add(num1, num2);
+            break;
+        case "−":
+            res = subtract(num1, num2);
+            break;
+        case "×":
+            res = multiply(num1, num2);
+            break;
+        case "÷":
+            res = divide(num1, num2);
+            break;
     }
+    return Math.round(res * 1000) / 1000;
 };
 
+const findOperator = function(string) {
+    for (let char of string.split("")) {
+        if (listOfOperators.includes(char)) return true;
+    }
+    return false;
+};
+
+let display = document.querySelector("#display-text");
+let buttons = document.querySelectorAll(".button");
+
+let firstNum = 0;
+let operator = "";
+let secondNum = 0;
+
+const populateDisplay = function(input) {
+    let displayText = display.textContent
+    //if input is an operator
+    if (findOperator(input)) {
+        if (findOperator(displayText)) {
+            if (typeof(Number(displayText.at(-1))) === "number" || displayText.at(-1) === ".") {
+                display.textContent = operate(firstNum, operator, secondNum) + input;
+                firstNum = operate(firstNum, operator, secondNum);
+                secondNum = 0;
+            } else {
+                display.textContent = displayText.slice(0,displayText.length - 1) + input;
+            }
+        } else if (displayText != "") {
+        display.textContent += input;
+        }
+        operator = input;
+    } else if (input === ".") {
+        if (findOperator(displayText)) {
+            if (!(secondNum.toString().split("").includes("."))) {
+                secondNum = secondNum + input;
+                display.textContent += input;
+            }
+        } else {
+            if (!(firstNum.toString().split("").includes("."))) {
+                firstNum = firstNum + input;
+                display.textContent += input;
+            }
+        }
+    } else {
+        display.textContent += input;
+        if (findOperator(displayText)) {
+            secondNum = secondNum + input;
+        } else {
+            firstNum = firstNum + input;
+        }
+    }
+    console.log([firstNum, operator, secondNum]);
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        populateDisplay(button.textContent);
+    });
+});
+
+const clearButton = document.querySelector("#clear");
+
+clearButton.addEventListener("click", () => {
+    display.textContent = "";
+    firstNum = 0;
+    operator = "";
+    secondNum = 0;
+});
+
+const equalButton = document.querySelector("#equals");
+
+equalButton.addEventListener("click", () =>{
+    let result = operate(firstNum, operator, secondNum);
+    if (result != undefined && result != NaN) {
+        display.textContent = result;
+        firstNum = result;
+        operator = "";
+        secondNum = 0;
+    }
+});
